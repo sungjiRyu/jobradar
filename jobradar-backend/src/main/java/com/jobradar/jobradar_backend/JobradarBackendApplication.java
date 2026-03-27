98c708c051fd7d124135d3202cdaedaafd603790
sungjiRyu <dhftlcm@gmail.com>
2026-03-27 12:20:19 +0900

    [chore] dev 브랜치 생성

---
+++ jobradar-backend/src/main/java/com/jobradar/jobradar_backend/JobradarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobradar.jobradar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobradarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobradarBackendApplication.class, args);
+	}
+
+}
---
+++ jobradar-backend/src/test/java/com/jobradar/jobradar_backend/JobradarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobradar.jobradar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobradarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
