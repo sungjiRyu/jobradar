43bf3992ab0ba503c0033ff640139058ee6726b6
sungjiRyu <dhftlcm@gmail.com>
2026-06-02 03:13:45 +0900

    [refacotr] 불필요한 코드 제거 (#99)

--- jobradar-backend/src/main/java/com/jobradar/backend/scrap/repository/ScrapRepository.java
+++ jobradar-backend/src/main/java/com/jobradar/backend/scrap/repository/ScrapRepository.java
@@ -4,8 +4,6 @@ import com.jobradar.backend.scrap.entity.Scrap;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
